e8ce1570a57565d28cb59e637191322759ceaf70
Anish Debnath <118295754+AnishDebnath@users.noreply.github.com>
2026-06-23 12:49:08 +0530

    Restructure Admin path for deployment

--- apps/admin/vite.config.ts
+++ apps/admin/vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ command }) => ({
-  base: '/admin/',
+  base: '/',
   plugins: [react()],
   build: {
     outDir: 'dist',
